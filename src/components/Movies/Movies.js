d9e16f714aea103dd4164bc47f512cf743036687
Nikolai Gavriushin <gna@live.ru>
2021-12-08 01:34:47 +0300

    feat: выполнить верстку формы поиска

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -3,10 +3,9 @@
 import './Movies.css';
 import Header from '../Header/Header';
 import Navigation from '../Header/Navigation/Navigation';
-import SearchForm from './SearchForm/SearchForm';
-import MoviesCardList from './MoviesCardList/MoviesCardList';
+import SearchForm from '../SearchForm/SearchForm';
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import Footer from '../Footer/Footer';
-
 
 function Movies() {
   return (
--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,10 +1,26 @@
 // компонент страницы с сохранёнными карточками фильмов
 
 import './SavedMovies.css';
+import Header from '../Header/Header';
+import Navigation from '../Header/Navigation/Navigation';
+import SearchForm from '../SearchForm/SearchForm';
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import Footer from '../Footer/Footer';
 
 function SavedMovies() {
   return (
-    <p>Компонент в разработке</p>
+    <>
+    <Header>
+      <Navigation />
+    </Header>
+
+    <main className="main">
+      <SearchForm />
+      <MoviesCardList />
+    </main>
+
+    <Footer />
+  </>
   );
 }
 
