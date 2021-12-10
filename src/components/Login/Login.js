12ee60bafda1616a29329f628890b10c02b2f827
Nikolai Gavriushin <gna@live.ru>
2021-12-10 20:55:15 +0300

    feat: выполнить верстку страниц авторизации и регистрации

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,24 @@
 // компонент страницы авторизации
 
 import './Login.css';
+import IdentityForm from '../IdentityForm/IdentityForm';
 
 function Login() {
   return (
-    <p>Компонент Login в разработке</p>
+    <IdentityForm
+      title='Рады видеть!'
+      submitButtonText='Войти'
+      navigationText='Ещё не зарегистрированы?'
+      link="/signup"
+      linkText="Регистрация"
+    >
+      <label className="identity-form__label">E-mail
+        <input type="email" className="identity-form__input" placeholder="введите e-mail" defaultValue="pochta@yandex.ru" />
+      </label>
+      <label className="identity-form__label">Пароль
+        <input type="password" className="identity-form__input" placeholder="введите пароль" />
+      </label>
+    </IdentityForm>
   );
 }
 
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,10 +1,28 @@
 // компонент страницы регистрации
 
 import './Register.css';
+import IdentityForm from '../IdentityForm/IdentityForm';
 
 function Register() {
   return (
-    <p>Компонент Register в разработке</p>
+    <IdentityForm
+      title='Добро пожаловать!'
+      submitButtonText='Зарегистрироваться'
+      navigationText='Уже зарегистрированы?'
+      link="/signin"
+      linkText="Войти"
+    >
+      <label className="identity-form__label">Имя
+        <input type="text" className="identity-form__input" placeholder="введите имя" defaultValue="Николай" />
+      </label>
+      <label className="identity-form__label">E-mail
+        <input type="email" className="identity-form__input" placeholder="введите e-mail" defaultValue="pochta@yandex.ru" />
+      </label>
+      <label className="identity-form__label">Пароль
+        <input type="password" className="identity-form__input" placeholder="введите пароль" />
+        <span className="identity-form__error">Что-то пошло не так...</span>
+      </label>
+    </IdentityForm>
   );
 }
 
